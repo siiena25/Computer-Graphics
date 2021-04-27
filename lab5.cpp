76e02c17db40934cad8867ee6649b213f60a0fff
siiena25 <yuliacoyulia@yandex.ru>
2021-04-27 11:15:48 +0300

    Update 5 lab

--- lab5.cpp
+++ lab5.cpp
@@ -29,6 +29,7 @@ std::vector<Point> clippedPoints;
 
 const int MAX_POINTS = 20;
 
+//Возвращает x-значение точки пересечения двух линий
 int x_intersect(int x1, int y1, int x2, int y2,
                 int x3, int y3, int x4, int y4) {
     int num = (x1*y2 - y1*x2) * (x3-x4) - (x1-x2) * (x3*y4 - y3*x4);
@@ -36,50 +37,12 @@ int x_intersect(int x1, int y1, int x2, int y2,
     return num/den;
 }
 
+//Возвращает y-значение точки пересечения двух линий.
 int y_intersect(int x1, int y1, int x2, int y2,
                 int x3, int y3, int x4, int y4) {
     int num = (x1*y2 - y1*x2) * (y3-y4) - (y1-y2) * (x3*y4 - y3*x4);
     int den = (x1-x2) * (y3-y4) - (y1-y2) * (x3-x4);
     return num/den;
-}
-
-void clip(int poly_points[][2], int &poly_size,
-          int x1, int y1, int x2, int y2) {
-    int new_points[MAX_POINTS][2], new_poly_size = 0;
-
-    for (int i = 0; i < poly_size; i++) {
-        int k = (i+1) % poly_size;
-        int ix = poly_points[i][0], iy = poly_points[i][1];
-        int kx = poly_points[k][0], ky = poly_points[k][1];
-        int i_pos = (x2-x1) * (iy-y1) - (y2-y1) * (ix-x1);
-        int k_pos = (x2-x1) * (ky-y1) - (y2-y1) * (kx-x1);
-
-        if (i_pos < 0  && k_pos < 0) {
-            new_points[new_poly_size][0] = kx;
-            new_points[new_poly_size][1] = ky;
-            new_poly_size++;
-        } else if (i_pos >= 0  && k_pos < 0) {
-            new_points[new_poly_size][0] = x_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
-            new_points[new_poly_size][1] = y_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
-            new_poly_size++;
-
-            new_points[new_poly_size][0] = kx;
-            new_points[new_poly_size][1] = ky;
-            new_poly_size++;
-        } else if (i_pos < 0  && k_pos >= 0) {
-            new_points[new_poly_size][0] = x_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
-            new_points[new_poly_size][1] = y_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
-            new_poly_size++;
-        } else {
-            //No points are added
-        }
-    }
-
-    poly_size = new_poly_size;
-    for (int i = 0; i < poly_size; i++) {
-        poly_points[i][0] = new_points[i][0];
-        poly_points[i][1] = new_points[i][1];
-    }
 }
 
 View view = View::PolygonOutline;
@@ -200,21 +163,71 @@ void DrawPolygonOutline(std::vector<Point> &curPoints, const unsigned char color
     }
 }
 
-void suthHodgClip(int poly_points[][2], int poly_size, int clipper_points[][2], int clipper_size) {
-    for (int i = 0; i < clipper_size; i++) {
-        int k = (i + 1) % clipper_size;
-        clip(poly_points, poly_size, clipper_points[i][0],
-             clipper_points[i][1], clipper_points[k][0],
-             clipper_points[k][1]);
+//Эта функция обрезает все края по отношению к одному краю отсечения области обрезки
+void clip(int poly_points[][2], int &poly_size, int x1, int y1, int x2, int y2) {
+    int new_points[MAX_POINTS][2], new_poly_size = 0;
+
+    //(ix, iy), (kx, ky) - значения координат точек
+    for (int i = 0; i < poly_size; i++) {
+        //i и k образуют линию в многоугольнике
+        int k = (i+1) % poly_size;
+        int ix = poly_points[i][0], iy = poly_points[i][1];
+        int kx = poly_points[k][0], ky = poly_points[k][1];
+        //Расчет позиции первой точки относительно отрезающей линии
+        int i_pos = (x2-x1) * (iy-y1) - (y2-y1) * (ix-x1);
+        ////Расчет позиции второй точки относительно отрезающей линии
+        int k_pos = (x2-x1) * (ky-y1) - (y2-y1) * (kx-x1);
+
+        //1 Случай: Обе вершины находятся внутри:
+        if (i_pos < 0  && k_pos < 0) {
+            //Точки не добавляются
+        }
+        //2 Случай: Первая вершина находится снаружи, а вторая - внутри:
+        else if (i_pos >= 0  && k_pos < 0) {
+            //Добавляется только точка пересечения с краем
+            new_points[new_poly_size][0] = x_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+            new_points[new_poly_size][1] = y_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+            new_poly_size++;
+        }
+            //3 Случай: Первая вершина находится внутри, а вторая - снаружи:
+        else if (i_pos < 0  && k_pos >= 0) {
+            //Добавляется точка пересечения с краем и вторая точка
+            new_points[new_poly_size][0] = x_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+            new_points[new_poly_size][1] = y_intersect(x1, y1, x2, y2, ix, iy, kx, ky);
+            new_poly_size++;
+
+            new_points[new_poly_size][0] = kx;
+            new_points[new_poly_size][1] = ky;
+            new_poly_size++;
+        }
+            //4 Случай: Обе вершины находятся снаружи:
+        else {
+            //Добавляется только вторая точка
+            new_points[new_poly_size][0] = kx;
+            new_points[new_poly_size][1] = ky;
+            new_poly_size++;
+        }
     }
 
     std::vector<Point> curPoints;
-    for (int i = 0; i < poly_size; i++) {
-        Point point = { poly_points[i][0], poly_points[i][1] };
+    for (int i = 0; i < new_poly_size; i++) {
+        Point point = { new_points[i][0], new_points[i][1] };
         curPoints.push_back(point);
     }
 
     DrawPolygonOutline(curPoints, drawClippedColor);
+}
+
+//Реализация алгоритма Сазерленда – Ходжмана
+void suthHodgClip(int poly_points[][2], int poly_size, int clipper_points[][2], int clipper_size) {
+    //i и k - два последовательных индекса
+    for (int i = 0; i < clipper_size; i++) {
+        int k = (i + 1) % clipper_size;
+        //Мы передаем текущий массив вершин, его размер и конечные точки выбранной линии отсечения
+        clip(poly_points, poly_size,
+             clipper_points[i][0], clipper_points[i][1],
+             clipper_points[k][0], clipper_points[k][1]);
+    }
 }
 
 void DrawClippedPolygonOutline() {
